5147d34027e92942e978251f8488be97372b89a7
White, Richard <richard.j.white@avanade.com>
2018-05-17 21:46:48 -0400

    Initial commit of the bot framework.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=urbanquery-Web-Deploy.pubxml -p:Password=jnNp8HRTetAb8W8FeurYQxlcaRDlqyuNfi5ny5eRCiCGrnq3Xhd7kNzRWsJQ
+
